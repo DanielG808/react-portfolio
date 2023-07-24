d6818cdc0a166eabf7816776554c0c3d5929c8ac
Dan G <daniel.w.gray1994@gmail.com>
2023-07-24 15:43:29 -0400

    added links to portfolio sections with images

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,7 +1,74 @@
 import React from "react";
+import Howie from "../assets/images/howie.png";
 
 const Portfolio = () => {
-  return <div id="portfolio">Portfolio</div>;
+  return (
+    <div
+      class="container d-flex flex-wrap justify-content-center"
+      id="portfolio"
+    >
+      <div className="card m-3" style={{ width: "18rem" }}>
+        <img src={Howie} className="card-img-top" alt="Howie" />
+        <div className="card-body">
+          <h5 className="card-title">Name of Project</h5>
+          <p className="card-text">Proj Desc</p>
+          <a href="#" className="btn btn-primary">
+            Link to Project
+          </a>
+        </div>
+      </div>
+      <div className="card m-3" style={{ width: "18rem" }}>
+        <img src={Howie} className="card-img-top" alt="Howie" />
+        <div className="card-body">
+          <h5 className="card-title">Name of Project</h5>
+          <p className="card-text">Proj Desc</p>
+          <a href="#" className="btn btn-primary">
+            Link to Project
+          </a>
+        </div>
+      </div>
+      <div className="card m-3" style={{ width: "18rem" }}>
+        <img src={Howie} className="card-img-top" alt="Howie" />
+        <div className="card-body">
+          <h5 className="card-title">Name of Project</h5>
+          <p className="card-text">Proj Desc</p>
+          <a href="#" className="btn btn-primary">
+            Link to Project
+          </a>
+        </div>
+      </div>
+      <div className="card m-3" style={{ width: "18rem" }}>
+        <img src={Howie} className="card-img-top" alt="Howie" />
+        <div className="card-body">
+          <h5 className="card-title">Name of Project</h5>
+          <p className="card-text">Proj Desc</p>
+          <a href="#" className="btn btn-primary">
+            Link to Project
+          </a>
+        </div>
+      </div>
+      <div className="card m-3" style={{ width: "18rem" }}>
+        <img src={Howie} className="card-img-top" alt="Howie" />
+        <div className="card-body">
+          <h5 className="card-title">Name of Project</h5>
+          <p className="card-text">Proj Desc</p>
+          <a href="#" className="btn btn-primary">
+            Link to Project
+          </a>
+        </div>
+      </div>
+      <div className="card m-3" style={{ width: "18rem" }}>
+        <img src={Howie} className="card-img-top" alt="Howie" />
+        <div className="card-body">
+          <h5 className="card-title">Name of Project</h5>
+          <p className="card-text">Proj Desc</p>
+          <a href="#" className="btn btn-primary">
+            Link to Project
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Portfolio;
