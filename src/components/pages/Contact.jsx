fcf93733df846e27b7f2a904ae0306a2dd0f336d
Dan G <daniel.w.gray1994@gmail.com>
2023-07-25 07:39:28 -0400

    added a form for contact info to be entered

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,7 +1,46 @@
 import React from "react";
 
 const Contact = () => {
-  return <div id="contact">Contact info: </div>;
+  return (
+    <div className="m-5" id="contact">
+      <h2 className="my-3">Fill out the form below to get in contact: </h2>
+      <form>
+        <div className="form-group">
+          <label htmlFor="exampleFormControlInput1">Name:</label>
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            placeholder="First and last name"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleFormControlInput1">Email address:</label>
+          <input
+            type="text"
+            className="form-control"
+            id="email"
+            placeholder="name@example.com"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleFormControlTextarea1">Message:</label>
+          <textarea
+            type="text"
+            className="form-control"
+            id="message"
+            rows={3}
+            placeholder="A breif description of your reason for reaching out"
+          />
+        </div>
+        <div className="form-group my-3">
+          <button type="submit" class="btn btn-primary mb-2">
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Contact;
