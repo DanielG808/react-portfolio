2bf0371088eea17d55e7285ffd4fcf0d0c16d24b
Dan G <daniel.w.gray1994@gmail.com>
2023-07-24 20:29:45 -0400

    added styling to the webpage

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,26 @@
+import GitHub from "../assets/images/GitHub_Logo.png";
+import LinkedIn from "../assets/images/LI-Logo.png";
+
 export default function Footer() {
   return (
     <footer>
-      <p className="bg-light">My Portfolio Footer</p>
+      <div className="footer-container">
+        <div className="img-container">
+          <a href="https://github.com/DanielG808" className="socials">
+            <img src={GitHub} alt="GitHub Profile Link" className="image m-5" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/dan-gray-90076617a/"
+            className="socials"
+          >
+            <img
+              src={LinkedIn}
+              alt="LinkedIn Profile Link"
+              className="image m-5"
+            />
+          </a>
+        </div>
+      </div>
     </footer>
   );
 }
--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -4,14 +4,14 @@ import Howie from "../../assets/images/Howie.png";
 const Portfolio = () => {
   return (
     <div
-      class="container d-flex flex-wrap justify-content-center"
+      class="container d-flex flex-wrap justify-content-evenly my-5"
       id="portfolio"
     >
       <div className="card m-3" style={{ width: "18rem" }}>
         <img src={Howie} className="card-img-top" alt="Howie" />
         <div className="card-body">
           <h5 className="card-title">Food for Thought</h5>
-          <p className="card-text">A recipe suggestion app</p>
+          <p className="card-text">A recipe suggestion app.</p>
           <a
             href="https://danielg808.github.io/food-for-thought/"
             className="btn btn-primary"
@@ -24,9 +24,24 @@ const Portfolio = () => {
         <img src={Howie} className="card-img-top" alt="Howie" />
         <div className="card-body">
           <h5 className="card-title">Movie Mentor</h5>
-          <p className="card-text">A Movie suggestion app</p>
+          <p className="card-text">A Movie suggestion app.</p>
           <a
             href="https://project-211-88db9fc198e3.herokuapp.com/login"
+            className="btn btn-primary"
+          >
+            Click Here!
+          </a>
+        </div>
+      </div>
+      <div className="card m-3" style={{ width: "18rem" }}>
+        <img src={Howie} className="card-img-top" alt="Howie" />
+        <div className="card-body">
+          <h5 className="card-title">Weather App</h5>
+          <p className="card-text">
+            An app that shows the weather and forecast near you.
+          </p>
+          <a
+            href="https://danielg808.github.io/weather-app/"
             className="btn btn-primary"
           >
             Click Here!
